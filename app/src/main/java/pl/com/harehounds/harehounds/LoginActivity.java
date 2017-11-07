f9ffb70e9b1cb0c5c510333b41f89046c2d21418
Michal Platta <m.platta@wp.pl>
2017-11-07 19:57:59 +0100

    remove not used lib

--- app/src/main/java/pl/com/harehounds/harehounds/LoginActivity.java
+++ app/src/main/java/pl/com/harehounds/harehounds/LoginActivity.java
@@ -4,7 +4,6 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
-import android.media.MediaCodec;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
