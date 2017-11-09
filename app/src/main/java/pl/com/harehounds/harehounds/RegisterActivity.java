4fe77d52057f3164f8aff8d3b80f417506bfa9e9
Michal Platta <m.platta@wp.pl>
2017-11-09 19:47:55 +0100

    cosmetic changes

--- app/src/main/java/pl/com/harehounds/harehounds/RegisterActivity.java
+++ app/src/main/java/pl/com/harehounds/harehounds/RegisterActivity.java
@@ -56,100 +56,10 @@ public class RegisterActivity extends Activity {
 		});
 	}
 
-	private void register() {
-		initialize();   // Initialize inputs to Strings;
-		if (!validate()) {
-			Toast.makeText(this, "Signup has failed :(", Toast.LENGTH_SHORT).show();
-			mEmailView.setText(email);
-			mNickNameView.setText(nickName);
-		} else {
-			showProgress(true);
-			onSignupSuccess();
-		}
-	}
-
-	private void onSignupSuccess() {
-		Response.Listener<String> responseListener = new Response.Listener<String>() {
-			@Override
-			public void onResponse(String response) {
-			try {
-				JSONObject jsonResponse = new JSONObject(response);
-				boolean loginSuccess = jsonResponse.getBoolean("loginSuccess");
-				boolean mailSuccess = jsonResponse.getBoolean("mailSuccess");
-
-				showProgress(false);
-				
-				if (loginSuccess) {
-					if (mailSuccess) {
-						successSignup();
-					} else {
-						mEmailView.requestFocus();
-						mEmailView.setError("E-mail already in use");
-					}
-				} else {
-					mNickNameView.requestFocus();
-					mNickNameView.setError("Login already in use");
-				}
-			} catch (JSONException e) {
-				// TODO: 31.10.2017 change error message 
-				showProgress(false);
-				AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-				builder.setMessage(e.getMessage())
-					.setNegativeButton("Retry", null)
-					.create()
-					.show();
-			}
-			}
-		};
-
-		RegisterRequest registerRequest = new RegisterRequest(email, nickName, password, responseListener);
-		RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
-		queue.add(registerRequest);
-	}
-
-	private void successSignup() {
-		Toast.makeText(this, "Signup success!", Toast.LENGTH_LONG).show();
-		this.finish();
-	}
-
-	private boolean validate() {
-		boolean valid = true;
-
-		if (!LoginActivity.isEmailValid(email) || email.length() > 32 || email.isEmpty()) {
-			mEmailView.setError("Invalid e-mail!");
-			valid = false;
-		}
-		if (nickName.isEmpty()) {
-			mNickNameView.setError(getString(R.string.error_field_required));
-		}
-		if (!(password.equals(confirm_password))) {
-			mConfirmPasswordView.setError(getString(R.string.prompt_password_confirm));
-			valid = false;
-		}
-		if (password.isEmpty()) {
-			mPasswordView.setError(getString(R.string.error_field_required));
-			valid = false;
-		}
-		if (confirm_password.isEmpty()) {
-			mConfirmPasswordView.setError(getString(R.string.error_field_required));
-			valid = false;
-		}
-
-		return valid;
-	}
-
-	// TODO: 01.11.2017 polecam zmienic nazwe tej funkcji na cos bardziej precyzyjnego
-	private void initialize() {
-		email = mEmailView.getText().toString().trim();
-		password = mPasswordView.getText().toString().trim();
-		confirm_password = mConfirmPasswordView.getText().toString().trim();
-		nickName = mNickNameView.getText().toString().trim();
-	}
-
 	/**
 	 * Shows the progress UI and hides the register form.
 	 */
-	// TODO: 01.11.2017 trzeba sprawdzic czy da sie to do jakiejs klasy statycznej czy dac 
+	// TODO: 01.11.2017 trzeba sprawdzic czy da sie to do jakiejs klasy statycznej czy dac
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
 	private void showProgress(final boolean show) {
 		// On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
@@ -181,5 +91,95 @@ public class RegisterActivity extends Activity {
 			mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
 			mRegisterFormView.setVisibility(show ? View.GONE : View.VISIBLE);
 		}
+	}
+
+	private void onSignUpSuccess() {
+		Response.Listener<String> responseListener = new Response.Listener<String>() {
+			@Override
+			public void onResponse(String response) {
+				try {
+					JSONObject jsonResponse = new JSONObject(response);
+					boolean loginSuccess = jsonResponse.getBoolean("loginSuccess");
+					boolean mailSuccess = jsonResponse.getBoolean("mailSuccess");
+
+					showProgress(false);
+
+					if (loginSuccess) {
+						if (mailSuccess) {
+							successSignUp();
+						} else {
+							mEmailView.requestFocus();
+							mEmailView.setError("E-mail already in use");
+						}
+					} else {
+						mNickNameView.requestFocus();
+						mNickNameView.setError("Login already in use");
+					}
+				} catch (JSONException e) {
+					// TODO: 31.10.2017 change error message
+					showProgress(false);
+					AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+					builder.setMessage(e.getMessage())
+							.setNegativeButton("Retry", null)
+							.create()
+							.show();
+				}
+			}
+		};
+
+		RegisterRequest registerRequest = new RegisterRequest(email, nickName, password, responseListener);
+		RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+		queue.add(registerRequest);
+	}
+
+	private boolean validate() {
+		boolean valid = true;
+
+		if (!LoginActivity.isEmailValid(email) || email.length() > 32 || email.isEmpty()) {
+			mEmailView.setError("Invalid e-mail!");
+			valid = false;
+		}
+		if (nickName.isEmpty()) {
+			mNickNameView.setError(getString(R.string.error_field_required));
+		}
+		if (!(password.equals(confirm_password))) {
+			mConfirmPasswordView.setError(getString(R.string.prompt_password_confirm));
+			valid = false;
+		}
+		if (password.isEmpty()) {
+			mPasswordView.setError(getString(R.string.error_field_required));
+			valid = false;
+		}
+		if (confirm_password.isEmpty()) {
+			mConfirmPasswordView.setError(getString(R.string.error_field_required));
+			valid = false;
+		}
+
+		return valid;
+	}
+
+	private void register() {
+		initialize();   // Initialize inputs to Strings;
+		if (!validate()) {
+			Toast.makeText(this, "Signup has failed :(", Toast.LENGTH_SHORT).show();
+			mEmailView.setText(email);
+			mNickNameView.setText(nickName);
+		} else {
+			showProgress(true);
+			onSignUpSuccess();
+		}
+	}
+
+	// TODO: 01.11.2017 polecam zmienic nazwe tej funkcji na cos bardziej precyzyjnego
+	private void initialize() {
+		email = mEmailView.getText().toString().trim();
+		password = mPasswordView.getText().toString().trim();
+		confirm_password = mConfirmPasswordView.getText().toString().trim();
+		nickName = mNickNameView.getText().toString().trim();
+	}
+
+	private void successSignUp() {
+		Toast.makeText(this, "Signup success!", Toast.LENGTH_LONG).show();
+		this.finish();
 	}
 }
