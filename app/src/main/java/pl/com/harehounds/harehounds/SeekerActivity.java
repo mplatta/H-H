ebabbe1e34324fe1c45f2cf00fee39561a51dd86
Michal Platta <m.platta@wp.pl>
2017-11-13 15:36:12 +0100

    add test seeker game view

---
+++ app/src/main/java/pl/com/harehounds/harehounds/SeekerActivity.java
@@ -0,0 +1,70 @@
+package pl.com.harehounds.harehounds;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SeekerActivity extends AppCompatActivity {
+
+	private TextView mDirection;
+	private Button mButton;
+	private LocationManager locationManager;
+	private LocationListener locationListener;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_seeker);
+
+		mDirection = (TextView) findViewById(R.id.direction);
+		mButton = (Button) findViewById(R.id.getLoc);
+
+		locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		locationListener = new LocationListener() {
+			@Override
+			public void onLocationChanged(Location location) {
+				mDirection.append("\n " + location.getLatitude() + "   " + location.getLongitude());
+			}
+
+			@Override
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+				mDirection.setText("zxc");
+			}
+
+			@Override
+			public void onProviderEnabled(String provider) {
+				mDirection.setText(provider);
+			}
+
+			@Override
+			public void onProviderDisabled(String provider) {
+				Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+				startActivity(intent);
+			}
+		};
+
+		if (ActivityCompat.checkSelfPermission(SeekerActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(SeekerActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			// TODO: Consider calling
+			//    ActivityCompat#requestPermissions
+			// here to request the missing permissions, and then overriding
+			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+			//                                          int[] grantResults)
+			// to handle the case where the user grants the permission. See the documentation
+			// for ActivityCompat#requestPermissions for more details.
+			return;
+		}
+
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
+		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 0, locationListener);
+		//mDirection.setText("test");
+	}
+}
---
+++ app/src/main/java/pl/com/harehounds/harehounds/SeekerLocationListener.java
@@ -0,0 +1,32 @@
+package pl.com.harehounds.harehounds;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+/**
+ * created by klata on 13.11.2017.
+ */
+
+class SeekerLocationListener implements LocationListener {
+
+	@Override
+	public void onLocationChanged(Location location) {
+
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+
+	}
+}
