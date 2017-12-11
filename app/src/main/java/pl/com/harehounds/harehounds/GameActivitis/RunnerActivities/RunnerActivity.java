eed7f12f5b7a8e5c00d5b2ba74dc8df42fb4856e
Adam Wejler <reljewa@op.pl>
2017-12-11 13:44:05 +0100

    Runner view wingow without any functionality yet

---
+++ app/src/main/java/pl/com/harehounds/harehounds/GameActivitis/RunnerActivities/RunnerActivity.java
@@ -0,0 +1,15 @@
+package pl.com.harehounds.harehounds.GameActivitis.RunnerActivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pl.com.harehounds.harehounds.R;
+
+public class RunnerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_runner);
+    }
+}
